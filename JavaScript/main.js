ef7b45df561459728db5da0e47f9d16cfad81a86
virgicont121 <virgicont1999@gmail.com>
2025-02-05 17:40:13 -0500

    Actualización: Sección Home Staging, Formulario y Footer

--- JavaScript/main.js
+++
@@ -1,9 +0,0 @@
-// Evento de scroll para agregar o quitar la clase 'scrolled' al header
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) { // Cambia el número para ajustar cuándo empieza el efecto
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
